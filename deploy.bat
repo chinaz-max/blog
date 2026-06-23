0299704dd9da4b1474796970b56b540ce50881b4
chinaz-max <3034772563@qq.com>
2026-06-23 19:36:49 +0800

    deploy: update blog

--- deploy.bat
+++ deploy.bat
@@ -5,12 +5,12 @@ cd /d "C:\Users\primer\Desktop\blog"
 C:\tool\Git\cmd\git.exe add -A
 
 if "%1"=="" (
-    C:\tool\Git\cmd\git.exe -c user.name=chinaz-max -c user.email=3034772563@qq.com commit -m "deploy: update blog"
+    C:\tool\Git\cmd\git.exe -c user.name=chinaz-max -c user.email=3034772563@qq.com -c http.proxy= commit -m "deploy: update blog"
 ) else (
-    C:\tool\Git\cmd\git.exe -c user.name=chinaz-max -c user.email=3034772563@qq.com commit -m "%*"
+    C:\tool\Git\cmd\git.exe -c user.name=chinaz-max -c user.email=3034772563@qq.com -c http.proxy= commit -m "%*"
 )
 
-C:\tool\Git\cmd\git.exe push origin main
+C:\tool\Git\cmd\git.exe -c http.proxy= push origin main
 
 echo.
 echo Done! Wait a few minutes and refresh your site.
